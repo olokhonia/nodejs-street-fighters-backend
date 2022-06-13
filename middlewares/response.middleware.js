5242177c6fd805b60ecc8947821916f4755c429c
Oleksandr Lokhonia <olokhonia@opengov.com>
2022-06-13 13:45:06 +0300

    implement response.middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,14 @@
 const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
+    // TODO: Implement middleware that returns result of the query
+    if (res.error) {
+        res.status(res.error.status || 400).send({
+            "error": true,
+            "message": res.error.message
+        });
+    } else {
+        res.status(200).send(res.data);
+    }
+
     next();
 }
 
